c46e6fd5a5039bbe1392df96d62bd843b2eda65e
AyanPaL7876 <ayanpal260@gmail.com>
2024-07-25 02:15:11 +0530

    mejor update

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# My Portfolio web page
-# Live url:https://AyanPaL7876.github.io/Portfolio_web_page/
+# Ayan's Personal Portfolio
+
+## My Portfolio Site 👉🏻 [Ayan Pal](https://AyanPaL7876.github.io/Portfolio_web_page/)
+
+
+## 🛠️ Features
+
+- **Responsive Design**: The UI adjusts for different screen sizes.
+- **Loading Indicator**: Shows a loading spinner while the QR code is being generated.
+- **Size Selection**: Users can select the desired size of the QR code.
+- **Downloadable QR Code**: The generated QR code can be downloaded as an image.
+
+## 🔧 Technologies Used
+
+![Languages](https://skillicons.dev/icons?i=html,css,tailwind,js)
+
+## 📸 Screenshots
+
+![Screenshot](./Assets/image.png)
+
+
+## 📧 Contact
+
+- **Name**: Ayan Pal
+- **GitHub**: [AyanPaL7876](https://github.com/AyanPaL7876)
+
--- code.js
+++ code.js
@@ -1,8 +1,9 @@
 const project = new Typed('#typing', {
     strings: [
-        `Ayan pal.`,
-        `A Web developer.`,
-        `A Programmer.`,
+        `Web Developer.`,
+        `Full stack Developer.`,
+        `Programmer.`,
+        `UI/UX Developer`
     ],
 
     typeSpeed: 70,
@@ -12,53 +13,6 @@ const project = new Typed('#typing', {
     
 });
 
-
-var hour = document.getElementById("hour");
-var minute = document.getElementById("minute");
-var seconds = document.getElementById("seconds");
-var set_clock = setInterval(
-function clock(){
-  var date_now = new Date();
-  var hr = date_now.getHours();
-  var min = date_now.getMinutes();
-  var sec = date_now.getSeconds();
-  var calc_hr = (hr * 30) + (min / 2);
-  var calc_min =    (min * 6)  ;
-  var calc_sec = sec * 6 ;
-  hour.style.transform = "rotate(" + calc_hr + "deg)";
-  minute.style.transform = "rotate(" + calc_min + "deg)";
-  seconds.style.transform = "rotate(" + calc_sec + "deg)";
-}, 1000);
-
-
-// define all UI variable
-const navToggler = document.querySelector('.nav-toggler');
-const navMenu = document.querySelector('.site-navbar ul');
-const navLinks = document.querySelectorAll('.site-navbar a');
-
-// load all event listners
-allEventListners();
-
-// functions of all event listners
-function allEventListners() {
-  // toggler icon click event
-  navToggler.addEventListener('click', togglerClick);
-  // nav links click event
-  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
-}
-
-// togglerClick function
-function togglerClick() {
-  navToggler.classList.toggle('toggler-open');
-  navMenu.classList.toggle('open');
-}
-
-// navLinkClick function
-function navLinkClick() {
-  if(navMenu.classList.contains('open')) {
-    navToggler.click();
-  }
-}
 
 window.oncontextmenu = () =>{
   return false;
